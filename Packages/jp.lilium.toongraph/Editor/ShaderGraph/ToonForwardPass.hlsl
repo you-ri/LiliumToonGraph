ce84b998481d35904bc936be29876d19849f875d
you-ri <you-ri@sasakingdom.com>
2020-10-26 23:05:57 +0900

    Fixed alpha calculation in transparency

--- Packages/jp.lilium.toongraph/Editor/ShaderGraph/ToonForwardPass.hlsl
+++ Packages/jp.lilium.toongraph/Editor/ShaderGraph/ToonForwardPass.hlsl
@@ -4,36 +4,36 @@
 void BuildInputData(Varyings input, float3 normal, out InputData inputData)
 {
     inputData.positionWS = input.positionWS;
-#ifdef _NORMALMAP
 
-#if _NORMAL_DROPOFF_TS
-	// IMPORTANT! If we ever support Flip on double sided materials ensure bitangent and tangent are NOT flipped.
-    float crossSign = (input.tangentWS.w > 0.0 ? 1.0 : -1.0) * GetOddNegativeScale();
-    float3 bitangent = crossSign * cross(input.normalWS.xyz, input.tangentWS.xyz);
-    inputData.normalWS = TransformTangentToWorld(normal, half3x3(input.tangentWS.xyz, bitangent, input.normalWS.xyz));
-#elif _NORMAL_DROPOFF_OS
-	inputData.normalWS = TransformObjectToWorldNormal(normal);
-#elif _NORMAL_DROPOFF_WS
-	inputData.normalWS = normal;
-#endif
-    
-#else
-    inputData.normalWS = input.normalWS;
-#endif
+    #ifdef _NORMALMAP
+        #if _NORMAL_DROPOFF_TS
+            // IMPORTANT! If we ever support Flip on double sided materials ensure bitangent and tangent are NOT flipped.
+            float crossSign = (input.tangentWS.w > 0.0 ? 1.0 : -1.0) * GetOddNegativeScale();
+            float3 bitangent = crossSign * cross(input.normalWS.xyz, input.tangentWS.xyz);
+            inputData.normalWS = TransformTangentToWorld(surfaceDescription.NormalTS, half3x3(input.tangentWS.xyz, bitangent, input.normalWS.xyz));
+        #elif _NORMAL_DROPOFF_OS
+            inputData.normalWS = TransformObjectToWorldNormal(surfaceDescription.NormalOS);
+        #elif _NORMAL_DROPOFF_WS
+            inputData.normalWS = surfaceDescription.NormalWS;
+        #endif
+    #else
+        inputData.normalWS = input.normalWS;
+    #endif
     inputData.normalWS = NormalizeNormalPerPixel(inputData.normalWS);
-    
+    inputData.viewDirectionWS = SafeNormalize(input.viewDirectionWS);
 
-#if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
-    inputData.shadowCoord = input.shadowCoord;
-#elif defined(MAIN_LIGHT_CALCULATE_SHADOWS)
-    inputData.shadowCoord = TransformWorldToShadowCoord(inputData.positionWS);
-#else
-    inputData.shadowCoord = float4(0, 0, 0, 0);
-#endif
-    
+    #if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+        inputData.shadowCoord = input.shadowCoord;
+    #elif defined(MAIN_LIGHT_CALCULATE_SHADOWS)
+        inputData.shadowCoord = TransformWorldToShadowCoord(inputData.positionWS);
+    #else
+        inputData.shadowCoord = float4(0, 0, 0, 0);
+    #endif
+
     inputData.fogCoord = input.fogFactorAndVertexLight.x;
     inputData.vertexLighting = input.fogFactorAndVertexLight.yzw;
     inputData.bakedGI = SAMPLE_GI(input.lightmapUV, input.sh, inputData.normalWS);
+    inputData.normalizedScreenSpaceUV = input.positionCS.xy;
 }
 
 PackedVaryings vert(Attributes input)
@@ -64,8 +64,8 @@ half4 frag(PackedVaryings packedInput) : SV_TARGET
     #endif
 
 #ifdef _ALPHAPREMULTIPLY_ON
-    surfaceDescription.BaseColor *= surfaceDescription.Alpha;
+    surfaceDescription.BaseColor *= alpha;
 #endif
 
-    return half4(surfaceDescription.BaseColor, surfaceDescription.Alpha);
+    return half4(surfaceDescription.BaseColor, alpha);
 }
